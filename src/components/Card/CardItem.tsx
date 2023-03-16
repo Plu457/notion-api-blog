f0e7268ccd058b29efb7338797cc1901c61f4ac4
plu457 <ekgus5095@gmail.com>
2023-03-16 22:09:12 +0900

    Style: framer-motion 라이브러리를 사용하여 카드에 대한 이펙트 추가

--- src/components/Card/CardItem.tsx
+++ src/components/Card/CardItem.tsx
@@ -12,34 +12,31 @@ const CardItem = ({ data }: CardItemProps) => {
   const { coverSrc, iconSrc, getImageSrc } = useImageLoading({ id, expiryTime, cover, icon });
 
   return (
-    <li className="list-none">
-      <article className="group">
-        <Link href={`/blog/${id}`}>
-          <a>
-            <div className="relative pt-[64%] overflow-hidden rounded-lg mb-4">
-              <Image
-                src={coverSrc}
-                alt={title}
-                layout="fill"
-                objectFit="cover"
-                className={`transition-all duration-300 group-hover:scale-110`}
-                onError={getImageSrc}
-                placeholder="blur"
-                blurDataURL={preview?.dataURIBase64 ?? Constant.IMAGE_LOADING_INDICATOR}
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <h2 className="text-2xl font-bold group-hover:text-blue-500">
-                <IconRenderer icon={iconSrc} />
-                {title}
-              </h2>
-              {description ? <p className="text-gray-700">{description}</p> : null}
-              <time className="font-light text-gray-500">{published}</time>
-            </div>
-          </a>
-        </Link>
-      </article>
-    </li>
+    <article className="transform transition-all duration-300 hover:-translate-y-2">
+      <Link href={`/blog/${id}`}>
+        <a>
+          <div className="relative pt-[64%] overflow-hidden rounded-lg mb-4">
+            <Image
+              src={coverSrc}
+              alt={title}
+              layout="fill"
+              objectFit="cover"
+              onError={getImageSrc}
+              placeholder="blur"
+              blurDataURL={preview?.dataURIBase64 ?? Constant.IMAGE_LOADING_INDICATOR}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <h2 className="text-2xl font-bold 0">
+              <IconRenderer icon={iconSrc} />
+              {title}
+            </h2>
+            {description ? <p className="text-gray-700">{description}</p> : null}
+            <time className="font-light text-gray-500">{published}</time>
+          </div>
+        </a>
+      </Link>
+    </article>
   );
 };
 
--- src/components/Card/CardList.tsx
+++ src/components/Card/CardList.tsx
@@ -1,11 +1,20 @@
+import { motion } from 'framer-motion';
 import CardItem from './CardItem';
 import { CardListProps } from './CardTypes';
 
 const CardList = ({ data }: CardListProps) => {
   return (
     <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-      {data.map(item => (
-        <CardItem key={item.id} data={item} />
+      {data.map((item, i) => (
+        <motion.li
+          key={item.id}
+          initial={{ opacity: 0, y: 8 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: (i + 1) * 0.1 }}
+          viewport={{ once: true }}
+        >
+          <CardItem key={item.id} data={item} />
+        </motion.li>
       ))}
     </ul>
   );

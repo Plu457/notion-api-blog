00a5dcabecf8b2be1d2b73255ecac7b6c620b4e1
plu457 <ekgus5095@gmail.com>
2023-03-17 22:37:59 +0900

    Refactor: 캐싱 관련 함수를 분리하여 getCachedDatabaseItems 함수 리팩터링

--- src/utils/getCachedDatabaseItems.ts
+++ src/utils/getCachedDatabaseItems.ts
@@ -4,38 +4,58 @@ import path from 'path';
 
 const OPTION_QUERY = 'option';
 
-const getCachedDatabaseItems = async ({ databaseId, options }: DatabaseOption) => {
-  if (process.env.NODE_ENV === 'development')
-    return await getDatabaseItems({ databaseId, options });
-
+const createCacheKey = (options?: string[]): string => {
   const cacheKey = new URLSearchParams({});
 
   if (options) cacheKey.append(OPTION_QUERY, JSON.stringify(options));
 
-  const CACHE_PATH = path.join(
-    __dirname,
-    `.collection${cacheKey.has(OPTION_QUERY) ? `?${cacheKey.toString()}` : ''}.json`,
-  );
+  return cacheKey.toString();
+};
 
-  let cachedData: Awaited<ReturnType<typeof getDatabaseItems>> = [];
+const getCachePath = (options?: string[]): string => {
+  const cacheKey = createCacheKey(options);
+  const cacheFileName = `.collection${cacheKey ? `_${cacheKey}` : ''}.json`;
 
+  return path.join(__dirname, cacheFileName);
+};
+
+const readCachedData = (cachePath: string): Awaited<ReturnType<typeof getDatabaseItems>> | null => {
   try {
-    const fileData = fs.readFileSync(CACHE_PATH, 'utf-8');
-    cachedData = JSON.parse(fileData);
+    const fileData = fs.readFileSync(cachePath, 'utf-8');
+    return JSON.parse(fileData);
+  } catch (error) {
+    if (error instanceof Error) console.error(error.message);
+    return null;
+  }
+};
+
+const writeCachedData = (
+  cachePath: string,
+  data: Awaited<ReturnType<typeof getDatabaseItems>>,
+): boolean => {
+  try {
+    if (!fs.existsSync(cachePath)) {
+      fs.writeFileSync(cachePath, JSON.stringify(data));
+      return true;
+    }
   } catch (error) {
     if (error instanceof Error) console.error(error.message);
   }
 
-  if (!cachedData.length) {
-    cachedData = await getDatabaseItems({ databaseId, options });
+  return false;
+};
 
-    try {
-      if (!fs.existsSync(CACHE_PATH)) {
-        fs.writeFileSync(CACHE_PATH, JSON.stringify(cachedData));
-      }
-    } catch (error) {
-      if (error instanceof Error) console.error(error.message);
-    }
+const getCachedDatabaseItems = async ({ databaseId, options }: DatabaseOption) => {
+  if (process.env.NODE_ENV === 'development') {
+    return await getDatabaseItems({ databaseId, options });
+  }
+
+  const cachePath = getCachePath(options);
+  let cachedData = readCachedData(cachePath);
+
+  if (!cachedData || !cachedData.length) {
+    cachedData = await getDatabaseItems({ databaseId, options });
+    writeCachedData(cachePath, cachedData);
   }
 
   return cachedData;

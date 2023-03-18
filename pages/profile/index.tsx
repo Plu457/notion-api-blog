bdfc2e39c70cce2ec80371c68f88ef08f09994b7
plu457 <ekgus5095@gmail.com>
2023-03-18 17:44:27 +0900

    Fixed: 프로필 페이지에서 누락된 revalidate 값 추가

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -32,6 +32,7 @@ export const getStaticProps: GetStaticProps<ProfilePageProps> = async () => {
 
     return {
       props: { recordMap },
+      revalidate: 60,
     };
   } catch (error) {
     throw new Error('Error while fetching profile page');

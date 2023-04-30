efd9c1997752929b655b916482a87242cd28f52d
plu457 <ekgus5095@gmail.com>
2023-04-30 16:32:57 +0900

    Fix: Duplicate atom key 관련 에러 처리. RecoilEnv 사용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import { RecoilRoot } from 'recoil';
+import { RecoilRoot, RecoilEnv } from 'recoil';
 import Layout from '@/components/Layout';
 
 import '@/styles/globals.css';
@@ -7,6 +7,8 @@ import 'katex/dist/katex.min.css'; //* 수학 공식 스타일
 import 'prismjs/themes/prism-tomorrow.css'; //* 코드 블럭 스타일
 import 'react-notion-x/src/styles.css';
 import '@/styles/notionStyle.scss';
+
+RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (

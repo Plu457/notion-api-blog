ee077ab44cba079ed39727428fdcc61aafc990d4
plu457 <ekgus5095@gmail.com>
2023-06-04 21:21:22 +0900

    Remove: 불필요한 커스텀 훅 파일 제거

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,4 @@
-export { default as useActiveTagList } from './Blog/useActiveTagList';
 export { default as useImageLoading } from './useImageLoading';
 export { default as useMenuOpen } from './useMenuOpen';
-export { default as useFilteredData } from './Blog/useFilteredData';
-export { default as usePostData } from './Blog/usePostData';
 export { default as useBlogNavigation } from './useBlogNavigation';
 export { default as useInitializeDataState } from './useInitializeDataState';

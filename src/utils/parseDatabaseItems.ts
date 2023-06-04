4e4553301a66e61ab46ff09435cf10bcfb30d736
plu457 <ekgus5095@gmail.com>
2023-06-04 21:47:03 +0900

    refactor: 데이터 파싱 함수에서 || 연산자를 활용해 각 필드에 대한 값 할당 로직을 간소화

--- src/utils/parseDatabaseItems.ts
+++ src/utils/parseDatabaseItems.ts
@@ -8,20 +8,16 @@ const parseDatabaseItems = (databaseItems: Awaited<ReturnType<typeof getDatabase
     const { Description, Published, Tags, Name } = item.properties;
 
     const cover =
-      item.cover?.type === 'external'
-        ? item.cover.external.url
-        : item.cover?.file
-        ? item.cover.file.url
-        : '';
+      item.cover?.type === 'external' ? item.cover.external.url : item.cover?.file?.url || '';
 
-    const published = Published?.type === 'date' ? Published.date?.start ?? '' : '';
+    const published = Published?.type === 'date' ? Published.date?.start || '' : '';
 
     const description =
-      Description?.type === 'rich_text' ? Description.rich_text[0]?.plain_text ?? '' : '';
+      Description?.type === 'rich_text' ? Description.rich_text[0]?.plain_text || '' : '';
 
     const tags = Tags?.type === 'multi_select' ? Tags.multi_select : [];
 
-    const title = Name?.type === 'title' ? Name.title[0].plain_text : '';
+    const title = Name?.type === 'title' ? Name.title[0]?.plain_text || '' : '';
 
     const expiryTime =
       item.cover?.type === 'file'

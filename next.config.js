b5891463329aabdeaae4a7bda86d7e0c254606da
plu457 <ekgus5095@gmail.com>
2023-07-17 19:05:07 +0900

    Feat: Next.js 이미지 설정 최적화

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ['s3.us-west-2.amazonaws.com', 'www.notion.so', 'images.unsplash.com'],
+    deviceSizes: [640, 750, 828, 1080, 1152],
   },
 };
 
